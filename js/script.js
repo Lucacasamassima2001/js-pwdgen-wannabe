c8d1dd174d27b8e8ddfc5105363d5d68c0a60715
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-04-04 13:40:47 +0200

    pass generator

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+
+const first_name = prompt("Come ti chiami?")
+
+const last_name = prompt("Qual è il tuo cognome?")
+
+const fav_colour = prompt("Qual è il tuo colore preferito?")
+
+password.innerHTML = first_name + last_name + fav_colour + "23"
